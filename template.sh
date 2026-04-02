d94c9096e3e5ee800f36252ce385cf407efcad9d
vishnupriyan207 <vishnupriyan207@gmail.com>
2026-04-02 22:30:58 +0530

    Added project structure and initial files

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src prompt.py
+touch .ene
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully!. "
